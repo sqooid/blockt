83d9965d2e1bdbe4507291adb441d3e8080042ed
Lucas <git@sl.thesqooid.com>
2024-10-14 14:15:04 +1100

    feat: fix current time bar overlap

--- src/lib/components/blocker/blockt-current-time-bar.svelte
+++ src/lib/components/blocker/blockt-current-time-bar.svelte
@@ -13,7 +13,7 @@
 	<div class="pointer-events-none absolute left-0 right-0" style:top={`${top}px`}>
 		<div
 			style:left={`${left - 6}px`}
-			class="bar muted absolute z-10 -translate-x-full -translate-y-1/2 rounded-md border bg-background p-[1px] pl-[2px] opacity-100 shadow-sm"
+			class="bar muted absolute z-40 -translate-x-full -translate-y-1/2 rounded-md border bg-background p-[1px] pl-[2px] opacity-100 shadow-sm"
 		>
 			{hourToReadable(hour)}
 		</div>
